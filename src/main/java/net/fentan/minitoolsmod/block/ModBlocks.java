d341b3a587965b576d564e19a41761bfe01606a4
Fentan-kyc <di-ma02@mail.ru>
2022-08-25 20:02:33 +0300

    Crystal trees

--- src/main/java/net/fentan/minitoolsmod/block/ModBlocks.java
+++ src/main/java/net/fentan/minitoolsmod/block/ModBlocks.java
@@ -1,12 +1,14 @@
 package net.fentan.minitoolsmod.block;
 
 import net.fentan.minitoolsmod.MiniToolsMod;
+import net.fentan.minitoolsmod.block.custom.ModRotatedPillarBlock;
 import net.fentan.minitoolsmod.item.ModCreativeModeTab;
 import net.fentan.minitoolsmod.item.ModItems;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -75,6 +77,83 @@ public class ModBlocks {
                             .strength(4f).requiresCorrectToolForDrops()),
                     ModCreativeModeTab.MOD_TOOLS);
 
+    //Trees
+    public static final RegistryObject<Block> PERIDOT_LOG =
+            registerBlock("peridot_log",
+                    () -> new ModRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_LOG)),
+                    ModCreativeModeTab.MOD_TOOLS);
+
+    public static final RegistryObject<Block> STRIPPED_PERIDOT_LOG =
+            registerBlock("stripped_peridot_log",
+                    () -> new ModRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.STRIPPED_OAK_LOG)),
+                    ModCreativeModeTab.MOD_TOOLS);
+
+    public static final RegistryObject<Block> PERIDOT_WOOD =
+            registerBlock("peridot_wood",
+                    () -> new ModRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_WOOD)),
+                    ModCreativeModeTab.MOD_TOOLS);
+
+    public static final RegistryObject<Block> STRIPPED_PERIDOT_WOOD =
+            registerBlock("stripped_peridot_wood",
+                    () -> new ModRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.STRIPPED_OAK_WOOD)),
+                    ModCreativeModeTab.MOD_TOOLS);
+
+    public static final RegistryObject<Block> PERIDOT_PLANKS =
+            registerBlock("peridot_planks",
+                    () -> new ModRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_PLANKS)),
+                    ModCreativeModeTab.MOD_TOOLS);
+
+
+    public static final RegistryObject<Block> RUBY_LOG =
+            registerBlock("ruby_log",
+                    () -> new ModRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_LOG)),
+                    ModCreativeModeTab.MOD_TOOLS);
+
+    public static final RegistryObject<Block> STRIPPED_RUBY_LOG =
+            registerBlock("stripped_ruby_log",
+                    () -> new ModRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.STRIPPED_OAK_LOG)),
+                    ModCreativeModeTab.MOD_TOOLS);
+
+    public static final RegistryObject<Block> RUBY_WOOD =
+            registerBlock("ruby_wood",
+                    () -> new ModRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_WOOD)),
+                    ModCreativeModeTab.MOD_TOOLS);
+
+    public static final RegistryObject<Block> STRIPPED_RUBY_WOOD =
+            registerBlock("stripped_ruby_wood",
+                    () -> new ModRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.STRIPPED_OAK_WOOD)),
+                    ModCreativeModeTab.MOD_TOOLS);
+
+    public static final RegistryObject<Block> RUBY_PLANKS =
+            registerBlock("ruby_planks",
+                    () -> new ModRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_PLANKS)),
+                    ModCreativeModeTab.MOD_TOOLS);
+
+
+    public static final RegistryObject<Block> SAPPHIRE_LOG =
+            registerBlock("sapphire_log",
+                    () -> new ModRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_LOG)),
+                    ModCreativeModeTab.MOD_TOOLS);
+
+    public static final RegistryObject<Block> STRIPPED_SAPPHIRE_LOG =
+            registerBlock("stripped_sapphire_log",
+                    () -> new ModRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.STRIPPED_OAK_LOG)),
+                    ModCreativeModeTab.MOD_TOOLS);
+
+    public static final RegistryObject<Block> SAPPHIRE_WOOD =
+            registerBlock("sapphire_wood",
+                    () -> new ModRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_WOOD)),
+                    ModCreativeModeTab.MOD_TOOLS);
+
+    public static final RegistryObject<Block> STRIPPED_SAPPHIRE_WOOD =
+            registerBlock("stripped_sapphire_wood",
+                    () -> new ModRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.STRIPPED_OAK_WOOD)),
+                    ModCreativeModeTab.MOD_TOOLS);
+
+    public static final RegistryObject<Block> SAPPHIRE_PLANKS =
+            registerBlock("sapphire_planks",
+                    () -> new ModRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_PLANKS)),
+                    ModCreativeModeTab.MOD_TOOLS);
 
 
 
